1010ef2ec5628cf9a4272244f40e0dafe3fa104c
anthrapperz <54307203+anthrapperz@users.noreply.github.com>
2019-08-26 18:38:43 +0530

    Update event_regex.ts

--- src/bot_utils/event_regex.ts
+++ src/bot_utils/event_regex.ts
@@ -7,7 +7,7 @@ export class EventRegex {
   readonly commandsRegexNoName: regexps.RegExps;
 
   constructor() {
-    var commands = ['^/start', '^/mirrorTar', '^/mirror', '^/mirrorStatus', '^/list', '^/getFolder', '^/cancelMirror', '^/cancelAll'];
+    var commands = ['^/start', '^/tar', '^/add', '^/Status', '^/list', '^/getFolder', '^/cancel', '^/cancelAll'];
     var commandsNoName: string[] = [];
     var commandAfter = ['$', ' (.+)', ' (.+)', '$', ' (.+)', '$', '$', '$'];
 
